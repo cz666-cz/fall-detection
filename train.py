2d68ae3cba84f4c1a7a1ca89ac1d02ee772e2841
cz666-cz <yunduo0827@gmail.com>
2025-12-29 09:55:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ from ultralytics import YOLO
 
 def main():
     # 使用绝对路径加载模型配置文件，训练之前记得注释掉损失函数
-    model = YOLO(r"D:\Downloads\yolov12-main\yolov12-main\LHFD-YOLO.yaml")
+    model = YOLO(r"D:\Downloads\yolov12-main\yolov12-main\CFD-YOLO.yaml")
 
     # 开始训练
     model.train(
-        data=r"D:\Downloads\yolov12-main\yolov12-main\ultralytics\cfg\datasets\Traffic accident FW.yaml",
+        data=r"D:\Downloads\yolov12-main\yolov12-main\ultralytics\cfg\datasets\Roboflow.yaml",
         epochs=300,
         seed=42,
         imgsz=640,
@@ -16,7 +16,7 @@ def main():
         device=0,
         workers=1,
         augment=False,  # 禁用图像增强
-        name="TFLHFD"  # 设置训练运行名称
+        name="RCFD-YOLO"  # 设置训练运行名称
     )
 
 
