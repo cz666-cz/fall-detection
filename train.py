add1fde9f1d3b62d6e9d9d45f108cd3652dfca0b
cz666-cz <yunduo0827@gmail.com>
2025-12-29 09:55:40 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,10 +3,9 @@ from ultralytics import YOLO
 
 
 def main():
-    # 使用绝对路径加载模型配置文件，训练之前记得注释掉损失函数
+   
     model = YOLO(r"D:\Downloads\yolov12-main\yolov12-main\CFD-YOLO.yaml")
 
-    # 开始训练
     model.train(
         data=r"D:\Downloads\yolov12-main\yolov12-main\ultralytics\cfg\datasets\Roboflow.yaml",
         epochs=300,
@@ -15,8 +14,8 @@ def main():
         batch=2,
         device=0,
         workers=1,
-        augment=False,  # 禁用图像增强
-        name="RCFD-YOLO"  # 设置训练运行名称
+        augment=False, 
+        name="RCFD-YOLO" 
     )
 
 
